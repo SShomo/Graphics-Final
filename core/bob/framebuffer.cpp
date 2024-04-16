bd5004038c12776cba55548a9393047d49b4bcab
sshomo <samantha.shomo@mymail.champlain.edu>
2024-04-16 10:07:18 -0400

    Added Assignment 1 in

---
+++ core/bob/framebuffer.cpp
@@ -0,0 +1,140 @@
+#include "framebuffer.h"
+#include <glm/glm.hpp>
+#include "external/glad.h"
+#include "external/stb_image.h"
+
+bob::Framebuffer bob::createFramebufferWithRBO(unsigned int width, unsigned int height, int colorFormat)
+{
+	bob::Framebuffer fbo;
+	fbo.width = width;
+	fbo.height = height;
+
+	glCreateFramebuffers(1, &fbo.fbo); //Create new frame buffer object
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo.fbo); // Bind the new FBO as the active one
+
+	//Color Buffer
+
+	glGenTextures(1, &fbo.colorBuffer[0]);
+	glBindTexture(GL_TEXTURE_2D, fbo.colorBuffer[0]);
+	glTexStorage2D(GL_TEXTURE_2D, 1, colorFormat, fbo.width, fbo.height);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fbo.colorBuffer[0], 0);
+
+	//Render Buffer
+	unsigned int rbo;
+	glGenRenderbuffers(1, &rbo);
+	glBindRenderbuffer(GL_RENDERBUFFER, rbo);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, fbo.width, fbo.height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rbo);
+
+	return fbo;
+}
+
+bob::Framebuffer bob::createFramebufferWithDepthBuffer(unsigned int width, unsigned int height, int colorFormat)
+{
+	bob::Framebuffer fbo;
+	fbo.width = width;
+	fbo.height = height;
+
+	glCreateFramebuffers(1, &fbo.fbo); //Create new frame buffer object
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo.fbo); // Bind the new FBO as the active one
+
+	//Color Buffer
+
+	glGenTextures(1, &fbo.colorBuffer[0]);
+	glBindTexture(GL_TEXTURE_2D, fbo.colorBuffer[0]);
+	glTexStorage2D(GL_TEXTURE_2D, 1, colorFormat, fbo.width, fbo.height);
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, fbo.colorBuffer[0], 0);
+
+
+	//Depth Buffer
+	glGenTextures(1, &fbo.depthBuffer);
+	glBindTexture(GL_TEXTURE_2D, fbo.depthBuffer);
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT16, fbo.width, fbo.height);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, fbo.depthBuffer, 0);
+
+	return fbo;
+}
+
+bob::Framebuffer bob::createShadowMapFramebuffer(unsigned int width, unsigned int height, int colorFormat)
+{
+	Framebuffer fbo;
+	fbo.width = width;
+	fbo.height = height;
+
+	glCreateFramebuffers(1, &fbo.fbo); //Create new frame buffer object
+	glBindFramebuffer(GL_FRAMEBUFFER, fbo.fbo); // Bind the new FBO as the active one
+
+	glGenTextures(1, &fbo.depthBuffer);
+	glBindTexture(GL_TEXTURE_2D, fbo.depthBuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, fbo.width, fbo.height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, fbo.depthBuffer, 0);
+
+	float borderColor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+	glDrawBuffer(GL_NONE);
+	glReadBuffer(GL_NONE);
+
+	return fbo;
+}
+
+bob::Framebuffer bob::createGBuffer(unsigned int width, unsigned int height)
+{
+	Framebuffer framebuffer;
+	framebuffer.width = width;
+	framebuffer.height = height;
+
+	glCreateFramebuffers(1, &framebuffer.fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+
+	int formats[3] = {
+		GL_RGB32F, //0 = World Position 
+		GL_RGB16F, //1 = World Normal
+		GL_RGB16F  //2 = Albedo
+	};
+
+	//Create 3 color textures
+	for (size_t i = 0; i < 3; i++)
+	{
+		glGenTextures(1, &framebuffer.colorBuffer[i]);
+		glBindTexture(GL_TEXTURE_2D, framebuffer.colorBuffer[i]);
+		glTexStorage2D(GL_TEXTURE_2D, 1, formats[i], width, height);
+		//Clamp to border so we don't wrap when sampling for post processing
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		//Attach each texture to a different slot.
+		//GL_COLOR_ATTACHMENT0 + 1 = GL_COLOR_ATTACHMENT1, etc
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, framebuffer.colorBuffer[i], 0);
+	}
+	//Explicitly tell OpenGL which color attachments we will draw to
+	const GLenum drawBuffers[3] = {
+			GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2
+	};
+	glDrawBuffers(3, drawBuffers);
+	//TODO: Add texture2D depth buffer
+	glGenTextures(1, &framebuffer.depthBuffer);
+	glBindTexture(GL_TEXTURE_2D, framebuffer.depthBuffer);
+	//Create 16 bit depth buffer - must be same width/height of color buffer
+	glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT16, width, height);
+	//Attach to framebuffer (assuming FBO is bound)
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, framebuffer.depthBuffer, 0);
+	
+	//TODO: Check for completeness
+	GLenum fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (fboStatus != GL_FRAMEBUFFER_COMPLETE) {
+		printf("Framebuffer incomplete: %d", fboStatus);
+	}
+
+	//Clean up global state
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	return framebuffer;
+}
---
+++ core/bob/framebuffer.h
@@ -0,0 +1,15 @@
+#include <glm/glm.hpp>
+
+namespace bob {
+	struct Framebuffer {
+		unsigned int fbo;
+		unsigned int colorBuffer[8];
+		unsigned int depthBuffer;
+		unsigned int width;
+		unsigned int height;
+	};
+	Framebuffer createFramebufferWithRBO(unsigned int width, unsigned int height, int colorFormat);
+	Framebuffer createFramebufferWithDepthBuffer(unsigned int width, unsigned int height, int colorFormat);
+	Framebuffer createShadowMapFramebuffer(unsigned int width, unsigned int height, int colorFormat);
+	Framebuffer createGBuffer(unsigned int width, unsigned int height);
+}
